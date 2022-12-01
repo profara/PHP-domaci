70f7f3ef71e0068814fd68f62e767de8effcef8e
Aleksandar Markovic <alekbg99@gmail.com>
2022-12-01 12:24:03 +0100

    pocetna stranica

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>">
+    <link href="https://www.dafontfree.net/embed/c3BlY2lmeS1wZXJzb25hbC1ub3JtYWwtYmxhY2smZGF0YS81Mi9zLzE1Njg5Ny9TcGVjaWZ5UEVSU09OQUwtTm9ybUJsYWNrLnR0Zg" rel="stylesheet" type="text/css" />
+
+    <title>Prijava</title>
+</head>
+
+<body>
+    <nav><a href="index.php">
+            <ul id="prva">
+                <li style="color:#FFD700"><b>blackberries</b></li>
+            </ul>
+        </a>
+
+    </nav>
+
+    <form action="#" method="post">
+        <div class="container">
+            <label class="username">Username</label>
+            <input type="text" name="username" class="form-control" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" name="password" class="form-control" required>
+            <button type="submit" name="submit">Sign In</button>
+        </div>
+    </form>
+
+
+</body>
+
+</html>
